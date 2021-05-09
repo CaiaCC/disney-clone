f2060c7e02c51acfd29799090e3f4e717c72bc36
Caia Chuang <caiachuang@gmail.com>
2021-05-08 22:57:42 -0700

    Get detail of movie from firebase.

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useDebugValue, useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import db from '../../firebase';
+
 import { AddList, Background, Container, ContentMeta, Controls, Description, GroupWatch, ImageTitle, Player, SubTitle, Trailer } from './StyledDetailElements';
 
 const Detail = () => {
+    const {id} = useParams();
+    const [detailData, setDetailData] = useState({});
+
+    useEffect(() => {
+        db.collection('movies')
+            .doc(id)
+            .get()
+            .then((doc) => {
+                if(doc.exists) {
+                    setDetailData(doc.data())
+                } else {
+                    console.log('No such document in firebase')
+                }
+            })
+            .catch(error => {
+                console.log('Error getting document: ', error)
+            });
+    },[id]);
+
     return (
         <Container>
             <Background>
-                <img src='' alt="" />
+                <img src={detailData.backgroundImg} alt={detailData.title} />
             </Background>
             <ImageTitle>
-                <img src='' alt="" />
+                <img src={detailData.titleImg} alt={detailData.title} />
             </ImageTitle>
             <ContentMeta>
                 <Controls>
                     <Player>
-                        <img src='/images/play-icon-black.png' alt="" />
+                        <img src="/images/play-icon-black.png" alt="" />
                         <span>Play</span>
                     </Player>
                     <Trailer>
-                        <img src='/images/play-icon-white.png' alt="" />
+                        <img src="/images/play-icon-white.png" alt="" />
                         <span>Trailer</span>
                     </Trailer>
                     <AddList>
@@ -26,19 +48,15 @@ const Detail = () => {
                     </AddList>
                     <GroupWatch>
                         <div>
-                            <img src='/images/group-icon.png' alt='' />
+                            <img src="/images/group-icon.png" alt="" />
                         </div>
                     </GroupWatch>
-                    <SubTitle>
-
-                    </SubTitle>
-                    <Description>
-                        
-                    </Description>
                 </Controls>
+                    <SubTitle>{detailData.subTitle}</SubTitle>
+                    <Description>{detailData.description}</Description>
             </ContentMeta>
         </Container>
-    )
+    );
 }
 
 export default Detail;
